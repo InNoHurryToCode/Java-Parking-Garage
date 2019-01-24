e88e0ee2cd27a1c0ed0453cae9d503e420326845
Merijn Hendriks <innohurrytocode@gmail.com>
2019-01-24 18:17:26 +0100

    Jar artifact initial project (#5)
    * Merged refactor-initial-project into jar-artifact-initial-project
    * Fixed missing semicolon
    * Format SimulationView
    * Added Jar Artifact for Simulation
    * Fix cars not drawn properly

--- src/Simulation/SimulatorView.java
+++ src/Simulation/SimulatorView.java
@@ -283,16 +283,12 @@ public class SimulatorView extends JFrame {
 
             Graphics graphics = carParkImage.getGraphics();
 
-            for(int floor = 0; floor < getNumberOfFloors(); ++floor) {
-                for(int row = 0; row < getNumberOfRows(); ++row) {
-                    for(int place = 0; place < getNumberOfPlaces(); ++place) {
+            for (int floor = 0; floor < getNumberOfFloors(); ++floor) {
+                for (int row = 0; row < getNumberOfRows(); ++row) {
+                    for (int place = 0; place < getNumberOfPlaces(); ++place) {
                         Location location = new Location(floor, row, place);
                         Car car = getCarAt(location);
-                        Color color =  Color.white
-
-                        if (car != null) {
-                            car.getColor();
-                        }
+                        Color color = car == null ? Color.white : car.getColor();
 
                         drawPlace(graphics, location, color);
                     }
