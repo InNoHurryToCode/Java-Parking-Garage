0a64366fff04b24d097abe0d64dcca0ce68a1c1e
Merijn Hendriks <innohurrytocode@gmail.com>
2019-01-24 18:15:54 +0100

    Fix cars not drawn properly

--- src/Simulation/SimulatorView.java
+++ src/Simulation/SimulatorView.java
@@ -288,11 +288,7 @@ public class SimulatorView extends JFrame {
                     for (int place = 0; place < getNumberOfPlaces(); ++place) {
                         Location location = new Location(floor, row, place);
                         Car car = getCarAt(location);
-                        Color color =  Color.white;
-
-                        if (car != null) {
-                            car.getColor();
-                        }
+                        Color color = car == null ? Color.white : car.getColor();
 
                         drawPlace(graphics, location, color);
                     }
