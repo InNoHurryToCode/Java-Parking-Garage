8fd6d1efd21d27597a6a0eadb82d44a340809345
Merijn Hendriks <innohurrytocode@gmail.com>
2019-01-24 22:08:51 +0100

    Add DaytimeManager

--- src/daytime/DaytimeManager.java
+++ src/daytime/DaytimeManager.java
@@ -1,4 +1,67 @@
 package daytime;
 
 public class DaytimeManager {
+    private Daytime daytime;
+
+    public DaytimeManager() {
+        this.daytime = new Daytime();
+    }
+
+    public void tick() {
+        this.updateSeconds();
+        this.updateMinutes();
+        this.updateHours();
+        this.updateDays();
+        this.updateWeeks();
+        this.updateMonths();
+        this.updateYears();
+    }
+
+    public void updateSeconds() {
+        ++this.daytime.seconds;
+    }
+
+    public void updateMinutes() {
+        if (this.daytime.seconds >= 60) {
+            ++this.daytime.minutes;
+            this.daytime.seconds = 0;
+        }
+    }
+
+    public void updateHours() {
+        if (this.daytime.minutes >= 60) {
+            ++this.daytime.hours;
+            this.daytime.minutes = 0;
+        }
+    }
+
+    public void updateDays() {
+        if (this.daytime.hours >= 24) {
+            ++this.daytime.days;
+            ++this.daytime.weekday;
+            this.daytime.hours = 0;
+        }
+    }
+
+    public void updateWeeks() {
+        if (this.daytime.weekday >= 7) {
+            ++this.daytime.weeks;
+            this.daytime.weekday = 0;
+        }
+    }
+
+    public void updateMonths() {
+        if (this.daytime.days > DaytimeUtility.getDaysInMonth(this.daytime)) {
+            ++this.daytime.months;
+            this.daytime.days = 1;
+        }
+    }
+
+    public void updateYears() {
+        if (this.daytime.months > 12) {
+            ++this.daytime.years;
+            this.daytime.weeks = 1;
+            this.daytime.months = 1;
+        }
+    }
 }
