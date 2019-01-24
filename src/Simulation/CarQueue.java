71f2440cbed85b1b6971c7474b7d0ef84f37b3d9
Merijn Hendriks <innohurrytocode@gmail.com>
2019-01-24 17:02:29 +0100

    Add JavaDoc to CarQueue

--- src/Simulation/CarQueue.java
+++ src/Simulation/CarQueue.java
@@ -2,17 +2,37 @@ package Simulation;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * CarQueue is a queue for cars
+ * @author Hanzehogeschool of Applied Sciences
+ */
 public class CarQueue {
     private Queue<Car> queue = new LinkedList<>();
 
+    /**
+     * Adds a car to the queue
+     * @author Hanzehogeschool of Applied Sciences
+     * @param car the car to add
+     * @return whenever the car is added to the queue
+     */
     public boolean addCar(Car car) {
         return queue.add(car);
     }
 
+    /**
+     * Removes a car fom the queue
+     * @author Hanzehogeschool of Applied Sciences
+     * @return the first car in the queue
+     */
     public Car removeCar() {
         return queue.poll();
     }
 
+    /**
+     * Get the amount of cars in the queue
+     * @author Hanzehogeschool of Applied Sciences
+     * @return the cars in the queue
+     */
     public int carsInQueue(){
     	return queue.size();
     }
